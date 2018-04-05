a3cbf675b63ad4ff838d3476484f2da79f1cf819
bagusrin <bagusrinn@gmail.com>
2018-04-05 11:37:11 +0700

    Update ProcessingText.php

--- src/ProcessingText.php
+++ src/ProcessingText.php
@@ -10,5 +10,21 @@ class ProcessingText{
 	public function processing(){
 		return "test";
 	}
+	
+	public function stemming(){
+
+		$stemming = new Stemmer();
+		$res = $stemming->stemming();
+
+		return $res;
+	}
+	
+	public function stopword(){
+
+		$stopwords = new Stopwords();
+		$res = $stopwords->removeWords();
+
+		return $res;
+	}
 
 }
